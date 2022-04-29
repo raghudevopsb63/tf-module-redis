58e394e0b4d5270e1e462e007d9dafff8de74479
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-29 06:25:37 +0530

    Update terraform/tf-module-redis/

--- cluster.tf
+++ cluster.tf
@@ -12,7 +12,7 @@ resource "aws_elasticache_cluster" "redis" {
 
 resource "aws_elasticache_parameter_group" "default" {
   name   = "roboshop-${var.ENV}"
-  family = "redis6.2"
+  family = "redis6.x"
 }
 
 resource "aws_elasticache_subnet_group" "subnet-group" {
