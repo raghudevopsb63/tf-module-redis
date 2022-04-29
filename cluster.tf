588f56df97d85f589792d157ff13578c3ab182e1
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-29 06:20:31 +0530

    Update terraform/tf-module-redis/

--- cluster.tf
+++ cluster.tf
@@ -7,6 +7,7 @@ resource "aws_elasticache_cluster" "redis" {
   engine_version       = "6.2"
   port                 = 6379
   subnet_group_name    = aws_elasticache_subnet_group.subnet-group.name
+  security_group_names = [aws_security_group.allow_redis.name]
 }
 
 resource "aws_elasticache_parameter_group" "default" {
@@ -17,4 +18,30 @@ resource "aws_elasticache_parameter_group" "default" {
 resource "aws_elasticache_subnet_group" "subnet-group" {
   name       = "roboshop-${var.ENV}"
   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
+}
+
+resource "aws_security_group" "allow_redis" {
+  name        = "roboshop-redis-${var.ENV}"
+  description = "roboshop-redis-${var.ENV}"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "roboshop-redis-${var.ENV}"
+  }
 }
