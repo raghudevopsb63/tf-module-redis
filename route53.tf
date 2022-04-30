fe3c22d2f6a8502063fce6c68c9e7c1765c3907e
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-30 06:50:23 +0530

    Update terraform/tf-module-redis/

--- route53.tf
+++ route53.tf
@@ -1,11 +1,7 @@
-//resource "aws_route53_record" "record" {
-//  zone_id = data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_ID
-//  name    = "redis-${var.ENV}.${data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_ZONE}"
-//  type    = "CNAME"
-//  ttl     = "300"
-//  records = [aws_elasticache_cluster.redis.]
-//}
-
-output "redis" {
-  value = aws_elasticache_cluster.redis
+resource "aws_route53_record" "record" {
+  zone_id = data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_ID
+  name    = "redis-${var.ENV}.${data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_ZONE}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [aws_elasticache_cluster.redis.cache_nodes[0].address]
 }
