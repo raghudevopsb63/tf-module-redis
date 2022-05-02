7fcb3602f7d6f9e61fc840ac61a93918e9048900
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-02 06:37:26 +0530

    Update terraform/tf-module-redis/

--- cluster.tf
+++ cluster.tf
@@ -1,18 +1,18 @@
 resource "aws_elasticache_cluster" "redis" {
   cluster_id           = "roboshop-${var.ENV}"
   engine               = "redis"
-  node_type            = "cache.t3.small"
-  num_cache_nodes      = 1
+  node_type            = var.ELASTICACHE_NODE_TYPE
+  num_cache_nodes      = var.ELASTICACHE_NODE_COUNT
   parameter_group_name = aws_elasticache_parameter_group.default.name
-  engine_version       = "6.x"
-  port                 = 6379
+  engine_version       = var.ELASTICACHE_ENGINE_VERSION
+  port                 = var.ELASTICACHE_PORT
   subnet_group_name    = aws_elasticache_subnet_group.subnet-group.name
   security_group_ids   = [aws_security_group.allow_redis.id]
 }
 
 resource "aws_elasticache_parameter_group" "default" {
   name   = "roboshop-${var.ENV}"
-  family = "redis6.x"
+  family = "redis${var.ELASTICACHE_ENGINE_VERSION}"
 }
 
 resource "aws_elasticache_subnet_group" "subnet-group" {
@@ -27,8 +27,8 @@ resource "aws_security_group" "allow_redis" {
 
   ingress {
     description = "TLS from VPC"
-    from_port   = 6379
-    to_port     = 6379
+    from_port   = var.ELASTICACHE_PORT
+    to_port     = var.ELASTICACHE_PORT
     protocol    = "tcp"
     cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
   }
--- vars.tf
+++ vars.tf
@@ -1,1 +1,5 @@
 variable "ENV" {}
+variable "ELASTICACHE_NODE_TYPE" {}
+variable "ELASTICACHE_NODE_COUNT" {}
+variable "ELASTICACHE_PORT" {}
+variable "ELASTICACHE_ENGINE_VERSION" {}
